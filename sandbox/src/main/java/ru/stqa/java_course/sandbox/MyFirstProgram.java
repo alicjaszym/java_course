39c1f86b523334dd77785d3ef1c4af6c98abde07
alicjaszym <58327760+alicjaszym@users.noreply.github.com>
2019-12-16 17:48:23 +0100

    added few functions

--- sandbox/src/main/java/ru/stqa/java_course/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java_course/sandbox/MyFirstProgram.java
@@ -3,6 +3,20 @@ package ru.stqa.java_course.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Alicja");
+
+    double a = 6;
+    double b = 4;
+      System.out.println("powierzchania kwadratu"+a+b+"="+area(a,b));
+  }
+
+  public static void hello(String somebody){
+      System.out.println("Hello, "+somebody+"!");
+  }
+
+  public static double area(double a, double b){
+    return   a*b;
   }
 }
