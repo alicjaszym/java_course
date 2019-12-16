fed3c21ac4b15743bebbb2078002f1b044576a66
alicjaszym <58327760+alicjaszym@users.noreply.github.com>
2019-12-16 19:43:06 +0100

    point function

--- sandbox/src/main/java/ru/stqa/java_course/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java_course/sandbox/MyFirstProgram.java
@@ -4,8 +4,10 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     Point v = new Point(3, 3);
-    System.out.println("distance " + v.p1 + " between " + v.p2+"="+v.distance());
+    System.out.println("distance " + v.p1 + " between " + v.p2+" = "+v.distance());
 
+    double a = 64;
+    System.out.println(Math.sqrt(a));
 
  /*   Square s = new Square(5);
     System.out.println("square" + s.l + "=" + s.area());
