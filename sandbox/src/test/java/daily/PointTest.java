fcba0617f66d95c277cdbe93ac1f1b2fe5f21a31
alicjaszym <58327760+alicjaszym@users.noreply.github.com>
2019-12-22 21:31:12 +0100

    Tests

--- sandbox/src/test/java/daily/PointTest.java
+++ sandbox/src/test/java/daily/PointTest.java
@@ -6,26 +6,35 @@ import org.testng.annotations.Test;
 public class PointTest {
 
 
-@Test
-public void Point(){
-  int x1=1;
-  int x2=2;
-  int y1=3;
-  int y2=4;
-  double dis;
-  dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-  System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
-  Assert.assertFalse(dis==5);
+  @Test
+  public void Point() {
+    int x1 = 1;
+    int x2 = 2;
+    int y1 = 3;
+    int y2 = 4;
+    double dis;
+    dis = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    System.out.println("distancebetween" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + dis);
+    Assert.assertTrue(dis == 1.4142135623730951);
   }
 
   @Test
   public void Points() {
-    int x=3;
-    int y=4;
+    int x = 3;
+    int y = 4;
     double dis;
-    dis=Math.sqrt((x-y)*(x-y));
-    System.out.println("distancebetween"+"("+x+","+y+")===>"+dis);
-    Assert.assertTrue(dis==1);
+    dis = Math.sqrt((x - y) * (x - y));
+    System.out.println("distancebetween" + "(" + x + "," + y + ")===>" + dis);
+    Assert.assertTrue(dis == 1);
+  }
+
+  @Test
+  public void ElsePoints() {
+    int x = 1589;
+    int y = 46;
+    double dis;
+    dis = Math.sqrt((x - y) * (x - y));
+    System.out.println("distancebetween" + "(" + x + "," + y + ")===>" + dis);
+    Assert.assertTrue(dis == 1543.0);
   }
 }
-
