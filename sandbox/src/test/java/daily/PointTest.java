ce502bcc8b5d7f21013d46f133e0fd17e9849732
alicjaszym <58327760+alicjaszym@users.noreply.github.com>
2020-01-06 15:03:41 +0100

    point

--- sandbox/src/test/java/daily/PointTest.java
+++ sandbox/src/test/java/daily/PointTest.java
@@ -11,7 +11,8 @@ public class PointTest {
   @Test
   public void Point() {
     Point p1 = new Point(1, 3);
+    Point p2 = new Point(4,3);
 
-  //  Assert.assertTrue(p1.distance(p2)==2);
+    Assert.assertTrue(p1.distance(p2)==3.0);
   }
 }
