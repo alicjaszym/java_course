b26f0130ff3ae6986e4eb80c8875c99fad5d0472
alicjaszym <58327760+alicjaszym@users.noreply.github.com>
2020-02-11 20:24:18 +0100

    http

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -64,4 +64,13 @@ public class ApplicationManager {
       return false;
     }
   }
+
+  public HttpSession newSession(){
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key){
+    return properties.getProperty(key);
+  }
+
 }
